657adaa762fa0853941f18e3c97f48d3e0217d8d
Noel Johnson <noel.johnson6213@gmail.com>
2022-10-02 07:46:20 +0530

    Continue chapter 1

---
+++ Chapter 01/getNumber.rb
@@ -0,0 +1,13 @@
+##GTG
+
+puts("Welcome to \'Get my Number!\'")
+print("Whats your name? ")
+
+input = gets()
+#inspect method is avaialable on every ruby object, it is used to print the object representation
+# you could use any of the following
+# 1) puts(obj.inspect())
+# 2) p(obj)
+puts("Welcome #{input}")
+
+##TYJC
