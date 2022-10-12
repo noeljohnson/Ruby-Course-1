917e52de14a718ed4099bd88a6bcdd0ea81c556e
Noel Johnson <noel.johnson6213@gmail.com>
2022-10-12 06:56:54 +0530

    Continue chapter 2

--- Chapter 02/gotAMotor.rb
+++ Chapter 02/gotAMotor.rb
@@ -8,9 +8,16 @@ def soundHorn()
   puts("Pressing on the horn button", "Beep beep!")
 end
 
-def useHeadlights(brightness)
+def useHeadlights(brightness = "low-beam")
   puts("Turning on #{brightness} headlight")
   puts("Watch out for the deer")
+end
+
+def mileage(milesDriven, gasUsed)
+  if (gasUsed == 0)
+    return 0.0
+  end
+  return (milesDriven / gasUsed)
 end
 
 accelerate()
