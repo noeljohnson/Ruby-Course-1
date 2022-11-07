0b055cb36010f448839dcc1422975136f74dcd3e
Noel Johnson <noel.johnson6213@gmail.com>
2022-11-07 14:10:40 +0530

    Continue chapter 3

---
+++ Chapter 03/animals.rb
@@ -0,0 +1,64 @@
+##GTG
+
+class Animal
+
+  attr_reader(:name, :age)
+  
+  def name=(value)
+    if (value == "")
+      raise("Invalid entry for name")
+    end
+    @name = value
+  end
+
+  def age=(value)
+    if (value < 0)
+      raise("Invalid entry for age")
+    end
+    @age = value
+  end
+
+  def talk()
+    puts("#{@name} says Bark!")
+  end
+
+  def move(destination)
+    puts("#{@name} runs to the #{destination}")
+  end
+
+  def reportAge()
+    puts("#{@name} is #{@age} years old")
+  end
+
+end
+
+class Bird < Animal
+  
+  def talk()
+    puts("#{@name} says Chirp! Chirp!")
+  end
+
+end
+
+class Dog < Animal
+
+end
+
+class Cat < Animal
+
+  def talk()
+    puts("#{@name} says Meow!")
+  end
+
+end
+
+class Armadillo < Animal
+
+  def move(destination)
+    puts("#{@name} unrolls!")
+    super(destination)
+  end
+
+end
+
+##TYJC
