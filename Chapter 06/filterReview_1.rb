76dcee076aec7f13ed28f12722f25e0a152fbd23
Noel Johnson <noel.johnson6213@gmail.com>
2022-12-03 11:56:31 +0530

    Continue chapter 6

---
+++ Chapter 06/filterReview_1.rb
@@ -0,0 +1,33 @@
+## GTG
+
+def getAdjective(review)
+  
+  revWords = review.split(' ');
+  adjIndex = (revWords.find_index("is") + 1)
+  return (revWords[adjIndex])
+
+end
+
+res = []
+
+File.open("reviews.txt"){ |file|
+  res = file.readlines()
+}
+
+rel_lines = res.find_all{ |line| 
+  (line.include?("Truncated"))
+}
+
+review = rel_lines.reject{ |line|
+  line.include?("--")
+}
+
+revAdj = review.map{ |line|
+  getAdjective(line)
+}
+
+p(revAdj)
+
+p(review)
+
+## TYJC
